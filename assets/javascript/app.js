335128c9676a5d909770b96393b3bc414b349925
Oliver Sun <olisun@mac.com>
2019-09-12 15:08:51 -0700

    fixed last bug with start stop gif function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ var topics = ['Avengers', 'Eternals', 'Marvel Phase Four', 'Captain America', 'B
 var newTopicButton = $('#new-topic-submitted');
 var avengerButtons = $('#avenger-buttons');
 var gifs = $('#forGifsOne');
-var marvelImage;
+
 
 // This assigns the apiCall function below to all buttons with a class of 'avenger-button'.
 $(document).on('click', '.avenger-button', apiCall);
@@ -39,17 +39,49 @@ function apiCall() {
       console.log(queryURL);
       console.log(response);
       var results = response.data;
-      for (var i = 0; i < results.length; i++) {
-        marvelImage = $('<img>');
-        var marvelDiv = $('<div>');
-        var p = $('<p>').text('Rating: ' + results[i].rating);
 
-        marvelImage.attr('src', results[i].images.fixed_height.url, 'data-still', 'data-animate', 'data-state');
-        marvelDiv.append(marvelImage);
-        marvelDiv.append(p);
-        gifs.prepend(marvelDiv);
+      // Creating a div to hold the movie
+
+
+      gifs.empty();
+
+      for (var i = 0; i < results.length; i++) {
+        var forGifsTwo = $('<div class="gify">');
+        var forGifsTwo = $('<div>');
+
+        forGifsTwo.attr('class', 'gify');
+
+        var marvelImage = $('<img>');
+
+        var rating = $('<p>').text('Rating: ' + results[i].rating);
+
+        marvelImage.attr('src', results[i].images.fixed_height.url, 'data-still', results[i].images.fixed_height_still.url, 'data-animate', results[i].images.fixed_height.url, 'data-state', 'still', 'class', 'gif');
+
+        // 'data-still=results[i].images.fixed_height_still.url', 'data-animate=results[i].images.fixed_height.url', 'data-state="still"', 'class="gif"'
+
+        // marvelImage.attr('src', results[i].images.fixed_height.url, 'data-still=results[i].images.fixed_height_still.url', 'data-animate=results[i].images.fixed_height.url', 'data-state="still"', 'class="gif"');
+
+        forGifsTwo.append(marvelImage);
+        forGifsTwo.append(rating);
+        gifs.prepend(forGifsTwo);
+
+
 
       }
     });
 
+};
+
+$(document).on('click', $(".gif"), pause);
+
+function pause() {
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    var animate = $(this).attr("data-animate");
+    $(this).attr("src", animate);
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
 };
--- assets/javascript/appTest.js
+++ assets/javascript/appTest.js
@@ -3,7 +3,7 @@ var topics = ['Avengers', 'Eternals', 'Marvel Phase Four', 'Captain America', 'B
 var newTopicButton = $('#new-topic-submitted');
 var avengerButtons = $('#avenger-buttons');
 var gifs = $('#forGifsOne');
-var marvelImage;
+
 
 // This assigns the apiCall function below to all buttons with a class of 'avenger-button'.
 $(document).on('click', '.avenger-button', apiCall);
@@ -48,22 +48,35 @@ function apiCall() {
       for (var i = 0; i < results.length; i++) {
         var forGifsTwo = $('<div class="gify">');
         var forGifsTwo = $('<div>');
+
         forGifsTwo.attr('class', 'gify');
 
-        marvelImage = $('<img>');
-        // var marvelDiv = $('<div>');
-        var p = $('<p>').text('Rating: ' + results[i].rating);
+        var marvelImage = $('<img>');
 
-        marvelImage.attr('src', results[i].images.fixed_height.url, 'data-still', 'data-animate', 'data-state');
+        var rating = $('<p>').text('Rating: ' + results[i].rating);
 
+        marvelImage.attr('src', results[i].images.fixed_height.url);
+        marvelImage.attr('data-animate', results[i].images.fixed_height.url);
+        marvelImage.attr('data-still', results[i].images.fixed_height_still.url);
+        marvelImage.attr('data-state', 'still');
+        marvelImage.addClass('gif')
 
         forGifsTwo.append(marvelImage);
-        forGifsTwo.append(p);
+        forGifsTwo.append(rating);
         gifs.prepend(forGifsTwo);
-
-
-
       }
     });
 
 };
+
+$(document).on('click', '.gif', function() {
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    var animate = $(this).attr("data-animate");
+    $(this).attr("src", animate);
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
